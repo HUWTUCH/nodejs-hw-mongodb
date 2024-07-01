c36433549af5e8e72bb25bfae86bce6692bafe27
Danik005 <141757644+HUWTUCH@users.noreply.github.com>
2024-07-02 00:41:51 +0300

    commit two

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (error) {
+    console.error('Failed to start application:', error);
+  }
 };
 
 bootstrap();
